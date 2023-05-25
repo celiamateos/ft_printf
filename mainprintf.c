6dccf957042f1f06595f898f28933963dfd41577
Celia Mateos Martin <cmateos-@c1r6s1.42madrid.com>
2023-05-25 22:25:26 +0200

    main para testear el proyecto, este no se entrega

---
+++ mainprintf.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainprintf.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmateos- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 15:00:16 by cmateos-          #+#    #+#             */
+/*   Updated: 2023/05/25 15:19:13 by cmateos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_printf.h"
+
+int	main(void)
+{
+	//char c = 'c';
+	//char *s = "soy una string";
+	//int d = 2432;
+	//void *ptr = "ke pasa";
+	char *none = NULL;
+
+
+	// ft_printf("custom --> %d", -12345);
+	// printf("\noriginal: %d\n", -12345);
+
+	// ft_printf("custom --> %c", c);
+	// printf("\noriginal:%c\n", c);
+	
+	// ft_printf("soy una string: %s", s);
+	// printf("\noriginal:%s\n", s);
+
+	// ft_printf("soy un digito: %d", d);
+	// printf("\noriginal:%d\n", d);
+
+	// printf("soy un int:%i", 123456);
+	// printf("\noriginal:%i\n", 123456);
+
+	// ft_printf("soy un puntero: %p", ptr);
+	// printf("\noriginal:%p\n", ptr);
+
+	// ft_printf("soy un porcentaje: %%");
+	// printf("\noriginal: %%\n");
+
+//	ft_printf("%s", none);
+//	printf("\n\n");
+//	printf("%d\t\t\n", ft_printf("las%%%d\n", 266861616));
+//	printf("%d\t\t\n", printf("las%%%d\n", 266861616));
+//	printf("\n\n");
+//	printf("%d\t\t\n", ft_printf("%d\n", -266861616));
+//	printf("%d\t\t\n", printf("%d\n", -266861616));
+//	printf("\n\n");
+//	printf("%d\t\t\n", ft_printf("%x\n", 266861616));
+//	printf("%d\t\t\n", printf("%x\n", 266861616));
+//	printf("\n\n");
+//	printf("%d\t\t\n", ft_printf("%X\n", 266861616));
+//	printf("%d\t\t\n", printf("%X\n", 266861616));
+
+//	ft_printf(" %u ", 9223372036854775807);
+//	printf("\n %u ", 9223372036854775807);
+
+//	ft_printf(" %u ", LONG_MIN);
+//	ft_printf(" %u ", 9223372036854775807LL);
+//	ft_printf(" %u %u %u %u %u %u %u", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
+
+//	ft_printf("%X%X%X", 100, 5442, -12321);
+//	printf("%X%X%X", 100, 5442, -12321);
+
+//	ft_printf("%s %d %p %% %x", "bonjour ", 42, &free, 42);
+//	printf("%s %d %p %% %x", "bonjour ", 42, &free, 42);
+
+//	ft_printf("%lX", 2147483647);
+//	printf("%lX", 2147483647);
+
+//	ft_printf("%d%d%d", -56410000, 654, 1);
+// ft_printf("b%p", 4294967295);
+
+//	ft_printf("%");
+//	printf("%");
+
+	return (0);
+}
